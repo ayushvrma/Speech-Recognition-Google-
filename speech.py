4665ea0501a972161234a753d86c239cd0817c02
Ayush Verma <ayush8402@gmail.com>
2020-09-23 19:53:41 +0530

    final changes

--- speech.py
+++ speech.py
@@ -22,4 +22,3 @@ with sr.Microphone(sample_rate=48000,chunk_size=2048) as source:
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service {0}".format(e))
 
-print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
